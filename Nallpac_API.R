6fc8b9eefcefd672b892266fb1709a8ec6db2c51
Maad Scientist MMXVIII <filorviridae@Maads-Mac-mini-2.local>
2023-08-19 18:56:34 -0400

    added function to retrieve info by SKU

---
+++ Nallpac_API.R
@@ -0,0 +1,11 @@
+# Loading packages
+library(httr)
+library(jsonlite)
+library(openxlsx)
+library(dplyr)
+
+#Retrieve product Information from NALPAC website. 
+
+#User authentication
+user<-'abc@xyzemail.com'
+pw<-'************************'
